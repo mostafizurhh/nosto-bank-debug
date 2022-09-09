d5fe9df2f66ca0b76e48c9753a11bcdab1c3e6ae
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-09 22:53:54 +0100

    fixed withdraw amount against balane total

--- js/withdraw.js
+++ js/withdraw.js
@@ -11,10 +11,13 @@
 
 document.getElementById("btn-withdraw").addEventListener("click", function () {
   const newWithdrawAmount = getInputFieldValueById("withdraw-field");
+  const previousBalanceTotal = getTextElementValueById("balance-total");
+  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+  if (newWithdrawAmount > newBalanceTotal || previousWithdrawTotal > newBalanceTotal) { return alert('not enough balance') };
+
   const previousWithdrawTotal = getTextElementValueById("withdraw-total");
   const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
   setTextElementValueById("withdraw-total", newWithdrawTotal);
-  const previousBalanceTotal = getTextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
   setTextElementValueById("balance-total", newBalanceTotal);
 });
